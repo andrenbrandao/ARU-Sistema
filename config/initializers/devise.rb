6d75c0434e8e00ead53f6c50743b07dd4a01fb22
Andre Brandao <andrenbrandao@gmail.com>
2013-01-10 01:19:12 -0200

    Senha esquecida tanto por Email quanto por Username

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -107,7 +107,7 @@ Devise.setup do |config|
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
-  # config.confirmation_keys = [ :email ]
+   config.confirmation_keys = [ :login ]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
@@ -163,7 +163,7 @@ Devise.setup do |config|
   # ==> Configuration for :recoverable
   #
   # Defines which key will be used when recovering the password for an account
-  # config.reset_password_keys = [ :email ]
+   config.reset_password_keys = [ :login ]
 
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
