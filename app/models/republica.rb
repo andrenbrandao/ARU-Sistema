857ba0b07092107e4ca269f31ddc454ef90e0fdc
Andre Brandao <andrenbrandao@gmail.com>
2013-01-10 01:15:00 -0200

    Autenticação com Usuário e Email ao mesmo tempo

--- app/models/republica.rb
+++ app/models/republica.rb
@@ -12,6 +12,10 @@ class Republica < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+  attr_accessible :login
   has_many :moradores, :dependent => :destroy, :inverse_of => :republica
 
 
@@ -27,48 +31,70 @@ class Republica < ActiveRecord::Base
   validates :ano_de_fundacao, presence: true
   validates :descricao, presence: true, length: {minimum: 140}
   validates :endereco, presence: true
-	#validates :telefone, presence: true
-	validates :username, presence: true, uniqueness: { case_sensitive: false }
+  #validates :telefone, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 
-	validate :min_of_moradores
-	validate :max_of_moradores
-	validate :has_one_representante
-	validates_confirmation_of :password
+  validate :min_of_moradores
+  validate :max_of_moradores
+  validate :has_one_representante
+  validates_confirmation_of :password
 
 
-	private
+  private
 
-	def copy_email_to_republica
-		self.moradores.reject(&:marked_for_destruction?).each do |f|
-			if f.representante == true
-				self.email = f.email
-			end
-		end
-	end
+  def copy_email_to_republica
+  	self.moradores.reject(&:marked_for_destruction?).each do |f|
+  		if f.representante == true
+  			self.email = f.email
+  		end
+  	end
+  end
 
-	def min_of_moradores
-		if self.moradores.reject(&:marked_for_destruction?).length < 3
-			self.errors.add(:base, "República deve ter no mínimo 3 moradores")
-		end
-	end
+  def min_of_moradores
+  	if self.moradores.reject(&:marked_for_destruction?).length < 3
+  		self.errors.add(:base, "República deve ter no mínimo 3 moradores")
+  	end
+  end
 
-	def max_of_moradores
-		if self.moradores.reject(&:marked_for_destruction?).length > 20
-			self.errors.add(:base, "República possui mais de 20 moradores")
-		end
-	end
+  def max_of_moradores
+  	if self.moradores.reject(&:marked_for_destruction?).length > 20
+  		self.errors.add(:base, "República possui mais de 20 moradores")
+  	end
+  end
 
-	def has_one_representante
-		count = 0
-		self.moradores.reject(&:marked_for_destruction?).each do |f|
-			if f.representante == true
-				count += 1
-			end
-		end
+  def has_one_representante
+  	count = 0
+  	self.moradores.reject(&:marked_for_destruction?).each do |f|
+  		if f.representante == true
+  			count += 1
+  		end
+  	end
 
-		if count == 0
-			self.errors.add(:base, "Selecione um representante")
-		end
-	end
+  	if count == 0
+  		self.errors.add(:base, "Selecione um representante")
+  	end
+  end
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+  	conditions = warden_conditions.dup
+  	if login = conditions.delete(:login)
+  		where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  	else
+  		where(conditions).first
+  	end
+  end
+
+	# function to handle user's login via email or username
+	# def self.find_for_database_authentication(warden_conditions)
+	# 	conditions = warden_conditions.dup
+	# 	if login = conditions.delete(:login).downcase
+	# 		where(conditions).where('$or' => [ {:username => /^#{Regexp.escape(login)}$/i}, {:email => /^#{Regexp.escape(login)}$/i} ]).first
+	# 	else
+	# 		where(conditions).first
+	# 	end
+	# end 
+
+
 
 end
--- test/unit/helpers/user_steps_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class UserStepsHelperTest < ActionView::TestCase
-end
