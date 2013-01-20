e35fc39dc67e9154b8d05992d586161581063695
Andre Brandao <andrenbrandao@gmail.com>
2013-01-20 02:51:20 -0200

    Consertado botao de Active da Navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -125,6 +125,14 @@ $(document).ready(function() {
   });
 });
 
+$('.navbar li a').click(function(e) {
+  var $this = $(this);
+  if (!$this.hasClass('active')) {
+    $this.addClass('active');
+  }
+  e.preventDefault();
+});
+
 // $(document).ready( function() {
 //   $('a#login-link').on('click', function() {
 //     $(this).modal('show');
