59ae4c9b89d7e96ba8f22c0ac09b0c1ccd897064
Andre Brandao <andrenbrandao@gmail.com>
2013-02-06 00:08:09 -0200

    Heroku ajuste

--- db/migrate/20130108010254_remove_numero_de_moradores_from_morador.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNumeroDeMoradoresFromMorador < ActiveRecord::Migration
-  def up
-    remove_column :moradores, :numero_de_moradores
-  end
-
-  def down
-    add_column :moradores, :numero_de_moradores, :integer
-  end
-end
