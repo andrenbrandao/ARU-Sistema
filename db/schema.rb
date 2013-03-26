3b82a85cfe1f240ecb152c330151ce199fabb969
Andre Brandao <andrenbrandao@gmail.com>
2013-03-26 00:00:54 -0300

    Consertado seed para Fog

--- db/schema.rb
+++ db/schema.rb
@@ -13,39 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130322234400) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "namespace"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-  end
-
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
-
   create_table "admins", :force => true do |t|
     t.string   "email",              :default => "", :null => false
     t.string   "encrypted_password", :default => "", :null => false
@@ -64,10 +31,6 @@ ActiveRecord::Schema.define(:version => 20130322234400) do
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
 
-  create_table "cursos", :force => true do |t|
-    t.string "nome"
-  end
-
   create_table "moradores", :force => true do |t|
     t.string   "nome"
     t.string   "sobrenome"
@@ -83,7 +46,6 @@ ActiveRecord::Schema.define(:version => 20130322234400) do
     t.boolean  "representante",   :default => false, :null => false
     t.string   "apelido"
     t.boolean  "exmorador",       :default => false, :null => false
-    t.integer  "curso_id"
     t.datetime "data_de_saida"
   end
 
