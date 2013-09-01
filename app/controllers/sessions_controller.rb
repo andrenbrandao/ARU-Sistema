40c42e5892a424d5bf68a18bbc70fd1a74038056
Andre Brandao <andrenbrandao@gmail.com>
2013-09-01 18:56:06 -0300

    Consertado links para dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < Devise::SessionsController
 
   # After SignIn path
   def after_sign_in_path_for(resource)
-  	admin_dashboard_path
+  	admin_dashboard_index_path
   end
 
 end
