4c86c8d7816e8d5b4b307746df36df90908f2a1d
André Brandão <andrenbrandao@gmail.com>
2016-02-01 12:58:39 -0200

    Fix factories for evento

--- spec/factories/eventos.rb
+++ spec/factories/eventos.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
   	nome 'InterReps'
   	ano 2014
   	open false
+  	terms_text "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   end
 
   factory :invalid_evento, parent: :evento do
